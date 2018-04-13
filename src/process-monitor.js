fa42594591a034629162d932599f3a4ac2eb70d6
Tyler Johnson <mail@tyler-johnson.ca>
2018-04-13 16:32:18 -0400

    Restart desktop software if player watchdog is down

---
+++ src/process-monitor.js
@@ -0,0 +1,44 @@
+const logger = require("./logger");
+const starter = require("./starter");
+const childProc = require("child_process");
+const defaultWindowsCommand = "wmic process get commandline |findstr asar\\\\watchdog";
+const defaultLinuxCommand = "ps -ax |grep asar/watchdog";
+const fiveMins = 300000;
+const defaultInterval = fiveMins;
+const isWindows = process.platform === "win32";
+const fiveSeconds = 5000;
+
+module.exports = {
+  init(interval = defaultInterval, scheduler = setInterval) {
+    scheduler(checkProcess.bind(null, scheduler), interval);
+  }
+};
+
+function checkProcess(scheduler) {
+  const command = isWindows ? defaultWindowsCommand : defaultLinuxCommand;
+  const options = {
+    timeout: fiveSeconds,
+    windowsHide: true
+  };
+
+  childProc.exec(command, options, (err, stdout, stderr)=>{ // eslint-disable-line max-statements
+    const found = err === null && stdout.includes("player-electron");
+    const notFound = !stdout.includes("player-electron");
+    const otherCommandError = err && err.code !== 1;
+    const stack = err && err.stack;
+
+    if (found) {return;}
+
+    if (otherCommandError) {
+      return logger.external("process monitor error", `${stack} ${stderr}`);
+    }
+
+    if (notFound) {
+      logger.external("no player watchdog", "restarting");
+      scheduler(starter.restart, fiveSeconds);
+      return;
+    }
+
+    logger.external("unexpected state", stdout || stderr);
+  });
+}
---
+++ test/unit/process-monitor.js
@@ -0,0 +1,107 @@
+/* eslint-env mocha */
+/* eslint-disable max-statements */
+const assert = require("assert");
+const simple = require("simple-mock");
+const childProc = require("child_process");
+
+const logger = require("../../src/logger");
+const starter = require("../../src/starter");
+const monitor = require("../../src/process-monitor");
+
+const processFoundStdoutWindows = "...player-electron.asar\\watchdog\\index.js";
+const processFoundStdoutLinux = "...player-electron.asar/watchdog/index.js";
+
+const scheduler = fn=>fn();
+
+describe("Process Monitor - Unit", ()=>{
+  beforeEach(()=>{
+    simple.mock(logger, "external").callFn(console.log);
+    simple.mock(starter, "restart").returnWith();
+  });
+
+  afterEach(()=>{
+    simple.restore()
+  });
+
+  it("should do nothing when process is present on linux", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb(null, `${cmd} ${processFoundStdoutLinux}`);
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.callCount, 0);
+    assert.equal(starter.restart.callCount, 0);
+  });
+
+  it("should do nothing when process is present on windows", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb(null, `${processFoundStdoutWindows}`);
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.callCount, 0);
+    assert.equal(starter.restart.callCount, 0);
+  });
+
+  it("should log and call restart when not found on linux", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb({code: 1}, `${cmd}`);
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.lastCall.args[0], "no player watchdog");
+    assert.equal(logger.external.callCount, 1);
+    assert.equal(starter.restart.callCount, 1);
+  });
+
+  it("should log and call restart when not found on windows", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb({code: 1}, "");
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.lastCall.args[0], "no player watchdog");
+    assert.equal(logger.external.callCount, 1);
+    assert.equal(starter.restart.callCount, 1);
+  });
+
+  it("should log when there's an unexpected command error on Linux", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb({code: 2}, "", "something bad");
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.lastCall.args[0], "process monitor error");
+    assert.equal(logger.external.callCount, 1);
+    assert.equal(starter.restart.callCount, 0);
+  });
+
+  it("should log and call restart when not found on windows", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb({code: 2}, "", "something bad");
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.lastCall.args[0], "process monitor error");
+    assert.equal(logger.external.callCount, 1);
+    assert.equal(starter.restart.callCount, 0);
+  });
+
+  it("should log unexpected state if stdout is empty after successful command", () => {
+    simple.mock(childProc, "exec").callFn((cmd, opts, cb)=>{
+      cb({code: 0}, "", "");
+    });
+
+    monitor.init(0, scheduler);
+
+    assert.equal(logger.external.lastCall.args[0], "process monitor error");
+    assert.equal(logger.external.callCount, 1);
+    assert.equal(starter.restart.callCount, 0);
+  });
+});
