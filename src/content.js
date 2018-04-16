f12d26841c6d048cf35ff5b3e0e1c6b9f9d7ce81
André Luiz Cardoso <andrecardoso@gmail.com>
2018-04-16 13:48:46 -0300

    Add white screen image comparison

--- src/content.js
+++ src/content.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
+const PNG = require("pngjs").PNG;
+const pixelmatch = require("pixelmatch");
 const messaging = require("common-display-module/messaging");
 const config = require("./config");
+const logger = require("./logger");
 
 function requestScreenshot() {
   return messaging.broadcastMessage({
@@ -8,6 +12,38 @@ function requestScreenshot() {
   });
 }
 
+function checkWhiteScreen(filePath) {
+  return readImage(filePath)
+    .then((image) => {
+      return [image, createWhiteImage(image)];
+    })
+    .then(([image, white]) => {
+      const diff = pixelmatch(image.data, white, null, image.width, image.height);
+      if (diff <= 0) {
+        logger.external("white screen detected");
+      }
+    })
+    .then(() => fs.unlinkSync(filePath))
+    .catch(() => fs.unlinkSync(filePath));
+}
+
+function readImage(filePath) {
+  return new Promise((resolve, reject) => {
+    const image = fs.createReadStream(filePath)
+      .pipe(new PNG())
+      .on('parsed', () => {
+        resolve(image);
+      })
+      .on('error', reject);
+  });
+}
+
+function createWhiteImage(image) {
+  const whitePixel = 255;
+  return Buffer.alloc(image.data.length, whitePixel);
+}
+
 module.exports = {
-  requestScreenshot
+  requestScreenshot,
+  checkWhiteScreen
 };
